0f7b245f075d0ff1c60d93b3adc014dfa57ea932
kitooth6 <kitooth666@icloud.com>
2025-07-07 22:56:35 +0900

    タスクステータスモデルの実装

--- backend/app/Models/TaskStatus.php
+++ backend/app/Models/TaskStatus.php
@@ -4,8 +4,39 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class TaskStatus extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'color',
+        'sort_order'
+    ];
+
+    /**
+     * Tasks
+     */
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class, 'status_id');
+    }
+
+    /**
+     * 並び順でソート
+     */
+    public function scopeOrdered($query)
+    {
+        return $query->orderBy('sort_order');
+    }
+
+    /**
+     * 名前でソート
+     */
+    public function scopeByName($query)
+    {
+        return $query->orderBy('name');
+    }
 }
